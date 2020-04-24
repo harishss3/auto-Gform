85db03a26bf3fb592207d95456b47b810a18bdc9
harishss3 <61965991+harishss3@users.noreply.github.com>
2020-04-24 17:58:34 +0530

    Update auto_script.py

--- auto_script.py
+++ auto_script.py
@@ -7,8 +7,8 @@
 import requests
 for i in range(100):
     url = 'https://docs.google.com/forms/d/e/1FAIpQLSdt4al2U8TTWbtjwYNGV3cSOdhaWdPu58QyRtjm0e7Z6M0FVw/formResponse'
-    form_data = {'entry.1156409':'Harish Adsule',
+    gform_data = {'entry.1156409':'Harish Adsule',
                 'entry.210055283':190070,
                 'entry.192452008':'Yes',
                 'entry.446099277':'https://github.com/harishss3/auto-Gform.git'}
-    r = requests.post(url, data=form_data)
+    r = requests.post(url, data=gform_data)
