7da0c75cbd4488546af5ea16e4e1a2ff6cfb6b76
Harish S. Adsule <61965991+harishss3@users.noreply.github.com>
2020-05-03 12:41:29 +0530

    Update auto_script.py

--- auto_script.py
+++ auto_script.py
@@ -5,10 +5,11 @@
 '''
 
 import requests
-for i in range(100):
-    url = 'https://docs.google.com/forms/d/e/1FAIpQLSdt4al2U8TTWbtjwYNGV3cSOdhaWdPu58QyRtjm0e7Z6M0FVw/formResponse'
-    gform_data = {'entry.1156409':'Harish Adsule',
-                'entry.210055283':190070,
-                'entry.192452008':'Yes',
-                'entry.446099277':'https://github.com/harishss3/auto-Gform.git'}
+    url = '' # Enter the URL of the google doc. Make sure to change 'viewform' to 'formResponse'
+    gform_data = {'entry.1':value1,
+                'entry.2':value2,
+                'entry.3':value3}      # Entry name can be found by inspecting the required element or from a pre-filled link 
     r = requests.post(url, data=gform_data)
+'''
+This can be used to fill a form multiple times by using a for loop. 
+'''
